67ec24108bf0203710cdf0c160ca3f636499607f
Laurent Vauthrin <lvauthrin@gmail.com>
2012-03-05 18:17:47 -0800

    Committed part 1

---
+++ Part1.rb
@@ -0,0 +1,52 @@
+# metaprogramming to the rescue!
+
+class Numeric
+  @@currencies = {'dollar' => 1, 'yen' => 0.0123, 'euro' =>  1.3225, 'rupee' => 0.020}
+  def method_missing(method_id)
+    singular_currency = method_id.to_s.gsub( /s$/, '')
+    if @@currencies.has_key?(singular_currency)
+      self * @@currencies[singular_currency]
+    else
+      super
+    end
+  end
+  
+  def in(currency)
+    singular_currency = currency.to_s.gsub( /s$/, '')
+    if @@currencies.has_key?(singular_currency)
+      self / @@currencies[singular_currency]
+    else
+      self
+    end
+  end
+  
+end
+
+# puts 5.dollars.in(:euros)
+# puts 10.euros.in(:rupees)
+# puts 200.yen.in(:dollars)
+
+class String
+  
+  def palindrome?
+    return false if self.nil?
+    cleanString = self.gsub(/\W/, '').downcase
+    return cleanString.eql?(cleanString.reverse)
+  end
+
+end
+
+# puts "Madam, I'm Adam!".palindrome?
+# puts "A man, a plan, a canal -- Panama".palindrome?
+# puts "Abracadabra".palindrome?
+
+module Enumerable
+  
+  def palindrome?
+    self.join.palindrome?
+  end
+  
+end
+
+# puts [1,2,3,2,1].palindrome?
+# puts [1,2,3,4,1].palindrome?
